f50f26ca7b8e17496b6b0e0dd88273ca9e5a2417
nimbus117 <mail@nimbus117.co.uk>
2018-10-26 19:07:17 +0100

    destructured family object in render method

--- client/src/views/instrument_family_view.js
+++ client/src/views/instrument_family_view.js
@@ -13,18 +13,21 @@ export default class InstrumentFamilyView {
   };
 
   render(family) {
+
+    const {name, description, instruments} = family;
+
     this.container.innerHTML = '';
 
-    const familyName = this.createElement('h2', family.name);
+    const familyName = this.createElement('h2', name);
     this.container.appendChild(familyName);
 
-    const familyDescription = this.createElement('p', family.description);
+    const familyDescription = this.createElement('p', description);
     this.container.appendChild(familyDescription);
 
     const instrumentListTitle = this.createElement('h3', 'Instruments include:');
     this.container.appendChild(instrumentListTitle);
 
-    const instrumentList = this.createInstrumentList(family.instruments);
+    const instrumentList = this.createInstrumentList(instruments);
     this.container.appendChild(instrumentList);
   };
 
